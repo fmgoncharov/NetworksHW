17a78730da2b5d0f3aab911b5edff7696ac2ce30
F G <f@g.com>
2022-12-09 17:06:12 +0300

    hw2

---
+++ hw2/mtu.py
@@ -0,0 +1,35 @@
+import subprocess
+import sys
+
+
+def main():
+    # Обработка исключения
+    if len(sys.argv) != 2:
+        raise RuntimeError('Wrong amount of arguments in main')
+    # Достаём хост из аргументов main
+    hostname = sys.argv[1]
+    # Задаём пороговые значения
+    mtu_L, mtu_R = 100, 2000
+    # Запускаем бинарный поиск по ответу на максимальный mtu
+    while mtu_R - mtu_L > 1:
+        mtu_M = (mtu_L + mtu_R) // 2
+        print("Check:", mtu_M)
+        # Пробуем пинг с заданным mtu
+        result = subprocess.run(
+            ['ping', hostname, '-M', 'do', '-s', str(mtu_M), '-c', '1'],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+        if result.returncode == 0:
+            mtu_L = mtu_M
+        elif result.returncode == 1:
+            mtu_R = mtu_M
+        else:
+            # Обработка исключения
+            raise RuntimeError('Error on ping')
+    # Печатаем максимальный подошедший mtu
+    print('Max MTU found:', mtu_L)
+
+
+if __name__ == "__main__":
+    main()
---
+++ hw2/readme.md
@@ -0,0 +1,16 @@
+# hw2
+
+## Инструкция по работе со скриптом
+
+### Запуск docker-контейнера
+
+```
+docker build -t mtu .
+docker run -i -t mtu
+```
+
+### Запуск скрипта
+
+```
+python3 mtu.py <host>
+```
